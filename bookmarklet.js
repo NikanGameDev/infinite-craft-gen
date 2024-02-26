b89edcf4a7c2f010b85f99cd9d49b5c8604343d4
NikanGameDev <87191838+NikanGameDev@users.noreply.github.com>
2024-02-26 07:57:31 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,11 +1,3 @@
-if (location.href == "https://nikangamedev.github.io/infinite-craft-gen/") {
-  const z = document.createElement("h1");
-  z.innerHTML = "Success! Bookmarklet works! Go try it on infinite craft now!";
-  document.body.append(z);
-}
-if (location.href != "https://neal.fun/infinite-craft/") {
-  alert("Please use on infinite craft website!");
-} 
 if (location.href == "https://neal.fun/infinite-craft/") {
   const a = document.createElement("button");
   a.innerHTML = "a";
