05440742870844f3c4b47ae85bee9af22a96018e
NikanGameDev <87191838+NikanGameDev@users.noreply.github.com>
2024-02-26 08:01:08 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,8 @@
 if (location.href == "https://neal.fun/infinite-craft/") {
   const a = document.createElement("button");
   a.innerHTML = "a";
+  a.addEventListener("click", function(){
+    // TODO: Add localStorage features
+  })
   document.body.append(a);
 }
